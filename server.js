a6e163aa2d9306b650a4ac69a067f7e37c35014a
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2022-06-17 23:31:59 +0300

    created home route through get request

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const app = express();
 
 require('dotenv').config();
 
+// middleware
+app.use(express.json());
+
 const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.status(200).json({ msg: 'success' });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
