926f32c31c6da8c4fc1d6e238000fd4f982a130b
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2022-06-17 23:48:40 +0300

    create post route to send data

--- server.js
+++ server.js
@@ -5,11 +5,18 @@ require('dotenv').config();
 
 // middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.status(200).json({ msg: 'success' });
+});
+
+app.post('/task', (req, res) => {
+  console.log(req.body);
+  const task = req.body;
+  res.status(201).json({ msg: 'success, data sent', data: task });
 });
 
 app.listen(port, () => {
