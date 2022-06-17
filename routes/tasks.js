ced6a726ed57c9be3618bcf5f0ae7b5d2addbe9c
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2022-06-18 00:02:51 +0300

    added new route to retrieve specific task

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,6 +5,14 @@ router.get('/', (req, res) => {
   res.status(200).json({ msg: 'success' });
 });
 
+router.get('/task/:id', (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  res
+    .status(200)
+    .json({ msg: 'success', data: `Retrieved Task Number: #${id}` });
+});
+
 router.post('/task', (req, res) => {
   console.log(req.body);
   const task = req.body;
